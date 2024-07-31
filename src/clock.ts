418a20133c7e8ee9f1bdc0cb5b02abecd5fa7d71
groomy790 <groomusemail@gmail.com>
2024-07-31 11:30:23 +0900

    FEAT: Add styling to clock feature

--- src/clock.ts
+++ src/clock.ts
@@ -4,17 +4,24 @@ const time = document.querySelector("#time") as HTMLSpanElement
 const AM_EMOJI = "☀️"
 const PM_EMOJI = "🌕"
 
-setInterval(() => {
+const ticking = () => {
     // get date info
     const date = new Date()
     
-    const h = date.getHours()
-    const m = date.getMinutes()
-    const s = date.getSeconds()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
     // show meridiem with emoji
-    meridiem.innerText = h > 12 ? PM_EMOJI : AM_EMOJI
+    meridiem.innerText = hour > 12 ? PM_EMOJI : AM_EMOJI
     
     // show time
-    time.innerText = `${h > 12 ? h - 12 : h}:${m}:${s}`
-}, 1000)
+    const format_hour = hour > 12 ? hour - 12 : hour
+    const format_minute = minute > 10 ? minute : `0${minute}`
+    const format_second = second > 10 ? second : `0${second}`
+
+    time.innerText = `${format_hour}:${format_minute}:${format_second}`
+}
+
+setInterval(ticking, 1000)
+ticking()
